1496d3064f9d45880533504b2a07252ead897b2f
Konr Larson <larsonkonr@gmail.com>
2014-09-24 23:49:00 -0600

    modified sequence test not running

--- test/sequence_test.rb
+++ test/sequence_test.rb
@@ -5,5 +5,8 @@ require 'minitest/pride'
 require_relative '../lib/sequence'
 
 class SequenceTest < Minitest::Test
-
+  def test_it_can_store_a_sequence
+    sequence = Sequence.new(["b", "y", "y", "y"])
+    assert.sequence = ["b", "y", "y", "y"]
+  end
 end
